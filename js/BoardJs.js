96b2ff15e7d63bd45540deee4569eca2da70be67
Narae Gwon <rnjs021031@gmail.com>
2023-05-23 22:19:43 +0900

    fourth commit

--- js/BoardJs.js
+++ js/BoardJs.js
@@ -1,1 +1,38 @@
-var name1 = 'kim';
+function AddPostit() {
+    // 포스트잇 객체 추가
+    var board = document.getElementById("board");
+    var newPostit = document.createElement("div");
+    newPostit.setAttribute("class", "postit");
+    board.appendChild(newPostit);
+    // 포스트잇 객체 디스플레이.. css?
+    // 텍스트 삽입
+}
+function DobbleClickPostit(text) {
+    // 더블클릭 시 함수 실행할거임 (호출 측에서 구현)
+    // alert? 아니면 노란 포스트잇 크게 확대
+    var body = document.getElementsByTagName("body");
+    var InptutPostit = document.createElement("div");
+    body.appendChild(InptutPostit);
+    // 글자수 체크
+    // 입력창 들이밀기 (확인 버튼)
+    var textArea = TextareaInPostit(InptutPostit);
+    InptutPostit.appendChild(textArea);
+    // (최대 입력 글자수 제한, overflow: hidden)
+}
+function CompletFixPostit(msg) {
+    // 바깥 화면 클릭하면 
+    // 입력 창 사라져
+    // 그 포스트잇의 입력값 반환 
+}
+function TextareaInPostit(postit) {
+    var textArea = document.createElement("textArea");
+    textArea.setAttribute("style", "height:300px; width:300px; resize: none;");
+    textArea.setAttribute("maxlength", "100");
+    textArea.setAttribute("placeholder", "텍스트를 입력하세요.");
+    $;
+    return textArea;
+}
+function init() {
+    var addButton = document.getElementById("addPostitButton");
+    addButton.addEventListener("click", AddPostit);
+}
--- js/BoardJs.ts
+++ js/BoardJs.ts
@@ -1,1 +1,48 @@
-let name1:string = 'kim'
+function AddPostit() { // 이벤트 함수, 포스트잇 객체를 동적으로 생성하는 함수
+    // 포스트잇 객체 추가
+    let board :HTMLElement = document.getElementById("board");
+    let newPostit :HTMLElement = document.createElement("div");
+    newPostit.setAttribute("class", "postit");
+    board.appendChild(newPostit);
+    
+    // 포스트잇 객체 디스플레이.. css?
+    // 텍스트 삽입
+    
+}
+
+function DobbleClickPostit(text :string) {
+    // 더블클릭 시 함수 실행할거임 (호출 측에서 구현)
+    // alert? 아니면 노란 포스트잇 크게 확대
+    let body = document.getElementsByTagName("body");
+    let InptutPostit :HTMLElement = document.createElement("div");
+    body.appendChild(InptutPostit);
+    // 글자수 체크
+    // 입력창 들이밀기 (확인 버튼)
+    let textArea :HTMLElement = TextareaInPostit(InptutPostit);
+    InptutPostit.appendChild(textArea);
+    // (최대 입력 글자수 제한, overflow: hidden)
+}
+
+
+function CompletFixPostit (msg) {
+    // 바깥 화면 클릭하면 
+        // 입력 창 사라져
+        // 그 포스트잇의 입력값 반환 
+}
+
+function TextareaInPostit(postit :HTMLElement) :HTMLElement{ // pure함수, 입력창을 생성하는 함수
+    let textArea :HTMLElement = document.createElement("textArea");
+    textArea.setAttribute("style", "height:300px; width:300px; resize: none;");
+    textArea.setAttribute("maxlength", "100");
+    textArea.setAttribute("placeholder", "텍스트를 입력하세요.");
+
+    $
+
+    return textArea;
+}
+
+function init() {
+    let addButton :HTMLElement = document.getElementById("addPostitButton");
+    addButton.addEventListener("click", AddPostit);
+
+}
