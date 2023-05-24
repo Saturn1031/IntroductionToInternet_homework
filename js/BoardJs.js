ee353e34a9b2fe88e6ea37b8c878009d3f13afa5
Narae Gwon <rnjs021031@gmail.com>
2023-05-25 01:03:31 +0900

    seventh commit

--- js/BoardJs.js
+++ js/BoardJs.js
@@ -1,17 +1,25 @@
 $(function () {
     $("#addPostitButton").on("click", function () {
-        $("#board").append("<div class='postit'><button class='del'><img src='' width='' alt='삭제버튼'></button></div>");
+        // 이벤트 함수, 포스트잇 객체를 동적으로 생성하고 보드의 자식 노드에 추가하는 함수
+        $("#board").append("<div class='postit'><div class='content'></div></button></div>");
     });
     $("#board").on("dblclick", ".postit", function () {
+        // 이벤트 함수, 포스트잇의 내용을 수정하는 함수
         var msg = prompt("방명록을 입력하세요.");
         var updatedPostit = editPostitContent(this, msg);
         $(this).replaceWith(updatedPostit);
     });
+    $("#delPostitButton").on("click", function () {
+        // 이벤트 함수, 마지막에 생성된 포스트잇 객체를 삭제하는 함수
+        var lastPostit = $(".postit:last");
+        lastPostit.remove();
+    });
 });
 function editPostitContent(element, content) {
-    var updatedElement = element.cloneNode(true); // 요소의 복제본 생성
-    updatedElement.textContent = content; // 복제본의 텍스트 업데이트
-    return updatedElement; // 업데이트된 요소 반환
+    // pure 함수, 포스트잇의 내용을 업데이트하고 포스트잇 객체를 리턴하는 함수
+    var updatedElement = element.cloneNode(true);
+    updatedElement.textContent = content;
+    return updatedElement;
 }
 /*
 function AddPostit() { // 이벤트 함수, 포스트잇 객체를 동적으로 생성하는 함수
