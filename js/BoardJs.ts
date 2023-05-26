092d804ff1d48374107e8fb22fdbbae7418df51f
Narae Gwon <rnjs021031@gmail.com>
2023-05-26 17:31:10 +0900

    포스트잇 수정할 때 위치 고정

--- js/BoardJs.ts
+++ js/BoardJs.ts
@@ -1,10 +1,10 @@
 $(function(){
     $("#addPostitButton").on("click", function(){  
         // 이벤트 함수, 포스트잇 객체를 동적으로 생성하고 보드의 자식 노드에 추가하는 함수
-        $("#board").append("<div class='postit'><div class='content'></div></button></div>");
+        $("#board").append("<div class='postit'><div class='content'></div></div>");
     });
 
-    $("#board").on("dblclick", ".postit", function() { 
+    $("#board").on("dblclick", ".content", function() { 
         // 이벤트 함수, 포스트잇의 내용을 수정하는 함수
         let msg: string | null = prompt("방명록을 입력하세요.");
         let updatedPostit: HTMLElement = editPostitContent(this, msg);
